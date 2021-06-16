581f5477b2c42b33d5408b0eb2e7868e632b5b05
Ramson <ramsonodoi@gmail.com>
2021-06-16 00:26:21 +0000

    initial commit of project

---
+++ GUISwingJPanel/src/Main.java
@@ -0,0 +1,56 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Panel;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		// JPanel = a GUI component that functions as a container to hold other components
+		
+		ImageIcon image = new ImageIcon("Calculator.png");
+		
+		JLabel label = new JLabel();
+		label.setText("Hi");
+		label.setIcon(image);
+		//label.setVerticalAlignment(JLabel.BOTTOM);
+		//label.setHorizontalAlignment(JLabel.RIGHT);
+		label.setBounds(0, 0, 75, 75);
+		
+		
+		JPanel redPanel = new JPanel ();
+		redPanel.setBackground(Color.red);
+		redPanel.setBounds(0, 0, 250, 250);
+		
+		JPanel bluePanel = new JPanel ();
+		bluePanel.setBackground(Color.blue);
+		bluePanel.setBounds(250, 0, 250, 250);
+		bluePanel.setLayout(null);
+		
+		JPanel greenPanel = new JPanel ();
+		greenPanel.setBackground(Color.green);
+		greenPanel.setBounds(0, 250, 500, 250);
+		greenPanel.setLayout(null);
+		
+		
+		
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setSize(750,750);
+		frame.setVisible(true);
+		frame.add(redPanel);
+		bluePanel.add(label);
+		frame.add(bluePanel);
+		frame.add(greenPanel);
+		
+
+	}
+
+}
